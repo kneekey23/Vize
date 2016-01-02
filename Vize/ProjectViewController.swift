a07df2f137c0e43ef6731f2c20d4d24b94674b01
kneekey23 <nicoleklein@me.com>
2016-01-02 13:49:27 -0800

    main layout of ui

---
+++ Vize/ProjectViewController.swift
@@ -0,0 +1,14 @@
+//
+//  ProjectViewController.swift
+//  Vize
+//
+//  Created by Nicki on 1/2/16.
+//  Copyright © 2016 shortkey. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class ProjectViewController: UIViewController{
+    
+}
